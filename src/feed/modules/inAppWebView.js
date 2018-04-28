50d7d5fd0398e009194dfaeba635eb704af416be
yuki2 <yuki2tech@gmail.com>
2018-04-28 23:32:16 +0900

    Add missing file

---
+++ src/feed/modules/inAppWebView.js
@@ -0,0 +1,28 @@
+import { Platform } from 'react-native';
+import { call, takeLatest } from 'redux-saga/effects';
+
+const inAppBrowser = Platform.select({
+  ios: require('NativeModules').InAppBrowser,
+});
+
+const OPEN_IN_APP_BROWSER = 'OPEN_IN_APP_BROWSER';
+
+export function openInAppBrowser(url) {
+  return {
+    type: OPEN_IN_APP_BROWSER,
+    payload: { url },
+  };
+}
+
+function* openInAppBrowserTask(action) {
+  try {
+    const { url } = action.payload;
+    yield call(inAppBrowser.show, url);
+  } catch (e) {
+    //
+  }
+}
+
+export function* subscribeOpenInAppBrowser() {
+  yield takeLatest(OPEN_IN_APP_BROWSER, openInAppBrowserTask);
+}
