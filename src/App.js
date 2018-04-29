b9628fa09023be5b6d0c6e8be7293d5947737a0b
yuki2 <yuki2tech@gmail.com>
2018-04-29 23:52:59 +0900

    Add Reactotron

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import { Navigation } from 'react-native-navigation';
 import { Provider } from 'react-redux';
+import './ReactotronConfig';
 
 import registerScreens from './registerScreens';
 import configureStore from './configureStore';
