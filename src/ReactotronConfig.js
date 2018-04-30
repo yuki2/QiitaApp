62d606e09bc0f6b27b8a51eaf4ef0932bab01b90
yuki2 <yuki2tech@gmail.com>
2018-04-30 20:19:07 +0900

    Fix lint rule

--- src/ReactotronConfig.js
+++ src/ReactotronConfig.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import Reactotron from 'reactotron-react-native';
 
 Reactotron.configure() // controls connection & communication settings
