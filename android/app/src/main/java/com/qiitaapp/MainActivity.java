65cf40d795e116cd9760b794c0892b26b4c64278
yuki2 <yuki2tech@gmail.com>
2018-05-03 14:11:07 +0900

    Fix android settings

--- android/app/src/main/java/com/qiitaapp/MainActivity.java
+++ android/app/src/main/java/com/qiitaapp/MainActivity.java
@@ -1,7 +1,16 @@
 package com.qiitaapp;
 
-import com.reactnativenavigation.controllers.SplashActivity;
 
-public class MainActivity extends SplashActivity {
+import com.facebook.react.ReactActivity;
 
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript.
+   * This is used to schedule rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "QiitaApp";
+  }
 }
